b21fca3faea727aa5cc2ebf6c3ebf547c71df0b3
Tomasz Trzciński <trzcinski.tomasz.1988@gmail.com>
2021-02-10 02:55:15 +0100

    Updated javalin.

--- src/main/java/HellOnWorld.java
+++ src/main/java/HellOnWorld.java
@@ -3,7 +3,7 @@ import io.javalin.Javalin;
 public class HellOnWorld {
 
 	public static void main(String[] args) {
-		Javalin app = Javalin.start(7000);
+		Javalin app = Javalin.create().start(7000);
 		app.get("/", ctx -> ctx.result("Hell-On-World"));
 	}
 
