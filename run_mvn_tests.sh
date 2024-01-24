cf0ca964fc967c314b0e0757fb0626a2b9f5af7a
Rzeznik <trzcinski.tomasz.1988@gmail.com>
2024-01-24 14:27:42 +0100

    Removed old slf4j lib.

--- run_mvn_tests.sh
+++ run_mvn_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-printf "\nWill now run JUnit tests in Javalin REST API.\n"
+printf "\nWill now run JUnit5 tests in Javalin REST API.\n"
 mvn test --batch-mode --fail-at-end
 printf "\nDONE.\n"
